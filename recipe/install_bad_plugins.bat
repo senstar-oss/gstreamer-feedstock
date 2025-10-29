4904e95bcb8fa40fb4b4e25dc14e225f19b93bf3
Norm Ross <norm.ross@senstar.com>
2025-10-29 12:55:07 -0400

    added plugins bad

---
+++ recipe/install_bad_plugins.bat
@@ -0,0 +1,28 @@
+@ECHO ON
+
+:: set pkg-config path so that host deps can be found
+:: (set as env var so it's used by both meson and during build with g-ir-scanner)
+set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
+
+:: get mixed path (forward slash) form of prefix so host prefix replacement works
+set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
+
+cd plugins_bad
+
+meson setup builddir ^
+    %MESON_ARGS% ^
+    --wrap-mode=nofallback ^
+    --prefix=%LIBRARY_PREFIX_M% ^
+    --backend=ninja ^
+    -Dexamples=disabled ^
+    -Dtests=disabled ^
+    -Dc_link_args=intl.dll.lib
+if errorlevel 1 exit 1
+
+ninja -v -C builddir -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja -C builddir install -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+del %LIBRARY_PREFIX%\bin\*.pdb
---
+++ recipe/install_bad_plugins.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+pushd plugins_bad
+
+mkdir build
+pushd build
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+EXTRA_FLAGS=""
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  # Use Meson cross-file flag to enable cross compilation
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+  if [[ "${target_platform}" == "osx-arm64" ]]; then
+      echo "objcpp = '${CXX}'" >> ${BUILD_PREFIX}/meson_cross_file.txt
+      cat ${BUILD_PREFIX}/meson_cross_file.txt
+  fi
+fi
+if [[ "${target_platform}" == "osx-"* ]]; then
+    export OBJCXX=${CXX}
+    export OBJCXX_FOR_BUILD=${CXX_FOR_BUILD}
+fi
+
+export PKG_CONFIG=$(which pkg-config)
+
+meson_options=(
+      -Dexamples=disabled
+      -Dtests=disabled
+)
+
+
+meson setup ${MESON_ARGS} \
+      $EXTRA_FLAGS \
+      --wrap-mode=nofallback \
+      "${meson_options[@]}" \
+      ..
+ninja -j${CPU_COUNT}
+ninja install
